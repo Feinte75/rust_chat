5bebfb75bf8bfe79e422bc8ded5ae9422bd5a174
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-11-21 12:16:11 -0500

    Implement non-blocking read from stdin

--- src/client/client.rs
+++ src/client/client.rs
@@ -5,12 +5,18 @@ use std::io;
 use std::io::{ErrorKind};
 use std::time::Duration;
 use std::thread;
+use std::sync::mpsc::{channel, Sender, TryRecvError};
 
-fn get_user_input() -> String {
-    
+fn get_user_input(tx : Sender<String>) {
+
     let mut buffer = String::new();
-    let _ = io::stdin().read_line(&mut buffer);
-    return buffer;
+    loop {
+
+        let _ = io::stdin().read_line(&mut buffer);
+        tx.send(buffer.clone());
+        buffer.clear();
+        thread::sleep_ms(50);
+    }
 }
 
 pub fn client() {
@@ -25,19 +31,44 @@ pub fn client() {
     let mut buf = [0;10];
     let _ = stream.read(&mut buf);
     let _ = stream.set_read_timeout(Some(Duration::from_millis(50))); 
-    loop {
-        let input_message = get_user_input();
+
+    let (tx, rx) = channel();
+
+    thread::spawn(move || 
         
+        get_user_input(tx.clone())
+    );
+
+    let mut input_message = String::new();
+    loop {
+
+        match rx.try_recv() {
+            Err(e) if e == TryRecvError::Empty => {
+                continue;
+            },
+
+            Err(e) => {
+                println!("{}", e);
+                break;
+            }
+            Ok(input) => {
+                input_message.push_str(str::from_utf8(input.as_bytes()).unwrap());        
+            }
+        }
+
         let _ = stream.write(&mut input_message.as_bytes());
 
         match stream.read(&mut buf) {
             Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
-            Err(_) => break,
+            Err(e) =>  {
+                println!("{}", e);
+                break;
+            },
             Ok(_) => ()
         }
 
         println!("Server response : {}", str::from_utf8(&buf).unwrap());
+        input_message.clear();
         thread::sleep_ms(100);
     }
-
 }
