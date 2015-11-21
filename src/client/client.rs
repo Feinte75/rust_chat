c40accafcd65051b550bcda75625d7cf6797d3a2
Feinte75 <glenn.feunteun@telecom-bretagne.eu>
2015-11-20 23:39:42 -0500

    Add non-blocking tcp stream read client side

--- src/client/client.rs
+++ src/client/client.rs
@@ -2,7 +2,9 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 use std::str;
 use std::io;
-
+use std::io::{ErrorKind};
+use std::time::Duration;
+use std::thread;
 
 fn get_user_input() -> String {
     
@@ -22,14 +24,20 @@ pub fn client() {
     println!("Reading !");
     let mut buf = [0;10];
     let _ = stream.read(&mut buf);
-
+    let _ = stream.set_read_timeout(Some(Duration::from_millis(50))); 
     loop {
         let input_message = get_user_input();
         
         let _ = stream.write(&mut input_message.as_bytes());
-        let _ = stream.read(&mut buf);
+
+        match stream.read(&mut buf) {
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => continue,
+            Err(_) => break,
+            Ok(_) => ()
+        }
 
         println!("Server response : {}", str::from_utf8(&buf).unwrap());
+        thread::sleep_ms(100);
     }
 
 }
